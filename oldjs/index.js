beac74c5f2e32ef616e5d80ccaed2d6c450992fa
ridomin <ridomin@live.com>
2020-04-19 18:28:37 -0700

    target ES2019

--- oldjs/index.js
+++ oldjs/index.js
@@ -1,2 +1,12 @@
-const rev = require('../dist/my-lib.js')
-console.log(rev.reverseString('hola'))
+const DTClient = require('../dist/dtclient.js').DTClient
+
+const connectionString = process.env.IOTHUB_CONNECTION_STRING
+
+async function main () {
+  const dtClient = new DTClient(connectionString)
+  const twinResponse = await dtClient.getDigitalTwin('rido-ppr-node')
+  const twin = twinResponse._response.parsedBody
+  console.log(twin)
+  console.log(twin.$metadata.$model)
+}
+main().catch(e => console.log(e))
