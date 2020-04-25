81fd28067e0f71e8694570f69ea30bc5fc95dad4
ridomin <ridomin@live.com>
2020-04-24 22:57:25 -0700

    tabs ts js

--- oldjs/index.js
+++ oldjs/index.js
@@ -2,17 +2,17 @@ const DTClient = require('../dist/dtclient.js').DTClient
 
 const connectionString = process.env.IOTHUB_CONNECTION_STRING
 
-const main= async () => {
-
+const main = async () => {
   const dtClient = new DTClient(connectionString)
   const twin = await dtClient.getDigitalTwin('rido-ppr')
-  
+
   console.log(twin.$metadata.$model)
 
-  for (t in twin){
+  for (const t in twin) {
     console.log(t)
     console.log(twin[t])
     console.log('------------------')
   }
 }
+
 main().catch(e => console.log(e))
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import {DTClient}  from './dtclient'
-//const connectionString : string = process.env.IOTHUB_CONNECTION_STRING as string
+import { DTClient } from './dtclient'
 
 const connectionString = process.env.IOTHUB_CONNECTION_STRING as string
 
-const main = async () =>{
-    let sc = new DTClient(connectionString)
-    let twin = await sc.getDigitalTwin("rido-ppr")
-    //console.log(twin)
-    console.log(twin.$metadata.$model)
-    
-    for (var dt in twin) {
-        console.log(dt)
-        console.log(twin[dt])
-    }
+const main = async () => {
+  const dtClient = new DTClient(connectionString)
+  const twin = await dtClient.getDigitalTwin("rido-ppr")
+
+  console.log(twin.$metadata.$model)
+
+  for (const t in twin) {
+    console.log(t)
+    console.log(twin[t])
+    console.log('------------------')
+  }
 }
 
-main().catch(e=>console.log(e))
+main().catch(e => console.log(e))
